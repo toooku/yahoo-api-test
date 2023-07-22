c6c7adf51b5c70088704ea1269d11702a8451782
tokunaga.tsukasa <96914086+tokunagatsukasa@users.noreply.github.com>
2023-07-22 20:25:37 +0900

    fix tailwindcss

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 9</title>
-    @vite('resources/sass/app.css')
+    @vite('resources/css/app.css')
 </head>
 <body>
 <div id="app"></div>
